9abc547dbf1458720c85dc72177ca43d37bf9385
bcoe <bencoe@gmail.com>
2011-11-29 12:48:58 -0800

    Switched to a process pool based approach to forking, server runs much much faster.

--- secure_smtpd/smtp_server.py
+++ secure_smtpd/smtp_server.py
@@ -1,15 +1,14 @@
 import secure_smtpd
-import ssl, smtpd, time, asyncore, os
+import ssl, smtpd, asyncore, socket
 from smtp_channel import SMTPChannel
-from multiprocessing import Process
 from asyncore import ExitNow
+from process_pool import ProcessPool
+from Queue import Empty
+from ssl import SSLError
 
 class SMTPServer(smtpd.SMTPServer):
     
-    KILL_SIGNAL = 9
-    MAXIMUM_EXECUTION_TIME = 20
-        
-    def __init__(self, localaddr, remoteaddr, ssl=False, certfile=None, keyfile=None, ssl_version=ssl.PROTOCOL_SSLv23, require_authentication=False, credential_validator=None, debug=False):
+    def __init__(self, localaddr, remoteaddr, ssl=False, certfile=None, keyfile=None, ssl_version=ssl.PROTOCOL_SSLv23, require_authentication=False, credential_validator=None, debug=False, maximum_execution_time=30, process_count=5):
         smtpd.SMTPServer.__init__(self, localaddr, remoteaddr)
         self.debug = debug
         self.certfile = certfile
@@ -19,65 +18,60 @@ class SMTPServer(smtpd.SMTPServer):
         self.require_authentication = require_authentication
         self.credential_validator = credential_validator
         self.ssl = ssl
+        self.maximum_execution_time = maximum_execution_time
+        self.process_pool = ProcessPool(self._accept_subprocess, process_count=process_count)
         
     def handle_accept(self):
         if self.debug:
             secure_smtpd.logger.info('handle_accept(): called.')
         
-        process = Process(target=self._accept_subprocess, args=[])
-        self.subprocesses.append({
-            'process': process,
-            'start_time': time.time()
-        })
-        process.start()
+        self.process_pool.handle_accept()
         
-        self._terminate_expired_subprocesses()
-        
-    def _terminate_expired_subprocesses(self):
-        current_time = time.time()
-        
-        def alive(process_dict):
-            if (current_time - process_dict['start_time']) > self.MAXIMUM_EXECUTION_TIME:
-                process_dict['process'].terminate()
-                return False
-            return process_dict['process'].is_alive()
+    def _accept_subprocess(self, queue):
+        while True:
             
-        self.subprocesses = filter(alive, self.subprocesses)
-        
-    def _accept_subprocess(self):
-        try:
-            pair = self.accept()
-            map = {}
+            try:
+                queue.get(block=True)
+            except Empty:
+                pass
             
-            if self.debug:
-                secure_smtpd.logger.info('_accept_subprocess(): smtp connection accepted within subprocess.')
-            
-            if pair is not None:
-                newsocket, fromaddr = pair
-                if self.ssl:
-                    newsocket = ssl.wrap_socket(
-                        newsocket,
-                        server_side=True,
-                        certfile=self.certfile,
-                        keyfile=self.keyfile,
-                        ssl_version=self.ssl_version,
-                    )
-                channel = SMTPChannel(
-                    self,
-                    newsocket,
-                    fromaddr,
-                    require_authentication=self.require_authentication,
-                    credential_validator=self.credential_validator,
-                    debug=self.debug,
-                    map=map
-                )
+            try:
+                pair = self.accept()
+                map = {}
                 
                 if self.debug:
-                    secure_smtpd.logger.info('_accept_subprocess(): starting asyncore within subprocess.')
+                    secure_smtpd.logger.info('_accept_subprocess(): smtp connection accepted within subprocess.')
                 
-                asyncore.loop(map=map)
-                secure_smtpd.logger.error('_accept_subprocess(): asyncore loop exited.')
-        except ExitNow:
-            if self.debug:
-                secure_smtpd.logger.info('_accept_subprocess(): smtp channel terminated asyncore.')
-            os.kill( os.getpid(), self.KILL_SIGNAL )
+                if pair is not None:
+                    
+                    newsocket, fromaddr = pair
+                    newsocket.settimeout(self.maximum_execution_time)
+                    
+                    if self.ssl:
+                        newsocket = ssl.wrap_socket(
+                            newsocket,
+                            server_side=True,
+                            certfile=self.certfile,
+                            keyfile=self.keyfile,
+                            ssl_version=self.ssl_version,
+                        )
+                    channel = SMTPChannel(
+                        self,
+                        newsocket,
+                        fromaddr,
+                        require_authentication=self.require_authentication,
+                        credential_validator=self.credential_validator,
+                        debug=self.debug,
+                        map=map
+                    )
+                    
+                    if self.debug:
+                        secure_smtpd.logger.info('_accept_subprocess(): starting asyncore within subprocess.')
+                    
+                    asyncore.loop(map=map)
+                    secure_smtpd.logger.error('_accept_subprocess(): asyncore loop exited.')
+            except (ExitNow, SSLError):
+                newsocket.shutdown(socket.SHUT_RDWR)
+                newsocket.close()
+                if self.debug:
+                    secure_smtpd.logger.info('_accept_subprocess(): smtp channel terminated asyncore.')            
