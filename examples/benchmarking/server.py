21e5ee2ad250c313ea0eb2c67c3d3cc32661d24d
Grant Edwards <grant.b.edwards@gmail.com>
2014-05-20 15:52:02 -0500

    Switch to non-privledged port to make testing easier.
    User the new server.run() method.

--- examples/benchmarking/server.py
+++ examples/benchmarking/server.py
@@ -1,37 +1,8 @@
-import asyncore,time,signal,sys
-from secure_smtpd import SMTPServer, FakeCredentialValidator
+from secure_smtpd import SMTPServer
 
 class SecureSMTPServer(SMTPServer):
-    
-    def __init__(self):
-        pass
-        
     def process_message(self, peer, mailfrom, rcpttos, message_data):
         pass
         
-    def start(self):
-        SMTPServer.__init__(
-            self,
-            ('0.0.0.0', 25),
-            None
-        )
-        asyncore.loop()
-        
-server = SecureSMTPServer()
-server.start()
-
-
-# normal termination of this process will kill worker children in
-# process pool so this process (the parent) needs to idle here waiting
-# for termination signal.  If you don't have a signal handler, then
-# Python multiprocess cleanup stuff doesn't happen, and children won't
-# get killed by sending SIGTERM to parent.
-
-def sig_handler(signal,frame):
-    print "Got signal %s, shutting down." % signal
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, sig_handler)
-
-while 1:
-    time.sleep(1)
+server = SecureSMTPServer(('0.0.0.0', 1025), None)
+server.run()
