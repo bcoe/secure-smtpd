5f39830d96091318d69ba68a6dfc819b08c7295d
bcoe <bencoe@gmail.com>
2014-05-20 23:39:38 -0700

    fix to port #.

--- examples/ssl_client.py
+++ examples/ssl_client.py
@@ -6,7 +6,7 @@ To: bar@localhost
 Here's my message!
 """
 
-server = smtplib.SMTP_SSL('localhost', port=465)
+server = smtplib.SMTP_SSL('localhost', port=1025)
 server.set_debuglevel(1)
 server.login('bcoe', 'foobar')
 server.sendmail('foo@localhost', ['bar@localhost'], msg)
